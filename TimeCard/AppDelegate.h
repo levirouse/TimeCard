28eb6077d89983aa31f44f42395ef06bcf00e437
Levi Rouse <Levi@Odysseus.local>
2013-09-12 13:58:41 -0700

    Have an actual working copy. Need to add open/save functionality.

--- TimeCard/AppDelegate.h
+++ TimeCard/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  TimeCard
 //
-//  Created by Levi on 9/12/13.
-//  Copyright (c) 2013 Levi. All rights reserved.
+//  Created by Levi Rouse on 9/12/13.
+//  Copyright (c) 2013 Levi Rouse. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
@@ -11,5 +11,19 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (strong) IBOutlet NSButton *clockInOutButton;
+@property (strong) IBOutlet NSTextField *commentField;
+@property (strong) IBOutlet NSTextView *timeCardView;
+
+- (IBAction) clockIn:  (id)sender;
+- (IBAction) clockOut: (id)sender;
+
+- (IBAction) openFile: (id)sender;
+- (IBAction) saveFile: (id)sender;
+
+- (void) appendToTimeCard: (NSString *) string;
+
+- (NSString *) currentDateTimeString;
 
 @end
--- TimeCard/AppDelegate.m
+++ TimeCard/AppDelegate.m
@@ -8,11 +8,75 @@
 
 #import "AppDelegate.h"
 
-@implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+@implementation AppDelegate  {
+    NSString *_clockedInAt;
 }
 
+- (void) applicationDidFinishLaunching: (NSNotification *) aNotification{
+    [_commentField setEditable: NO];
+}
+
+- (BOOL) applicationShouldTerminateAfterLastWindowClosed: (NSApplication *) app{
+    return YES;
+}
+
+- (IBAction) clockIn: (id)sender {
+    _clockedInAt = [self currentDateTimeString];
+    [self appendToTimeCard: _clockedInAt];
+    
+    [_commentField setEditable: YES];
+    [[_commentField window] makeFirstResponder: _commentField];
+    
+    [_clockInOutButton setTitle: @"Clock Out"];
+    [_clockInOutButton setAction: @selector(clockOut:)];
+}
+
+- (IBAction) clockOut: (id)sender {
+    _clockedInAt = [self currentDateTimeString];
+    
+    NSString *comment = _commentField.stringValue;
+    if (comment.length == 0) {
+        comment = @"[No comment]";
+    }
+    
+    NSString *timeCardString = [[NSString alloc] initWithFormat: @", %@, %@\n", _clockedInAt, comment];
+    
+    [self appendToTimeCard: timeCardString];
+    
+    [_commentField setStringValue: @""];
+    [_commentField setEditable: NO];
+    
+    [_clockInOutButton setTitle: @"Clock In"];
+    [_clockInOutButton setAction: @selector(clockIn:)];
+    [[_clockInOutButton window] makeFirstResponder: _clockInOutButton];
+}
+
+- (IBAction) openFile: (id)sender {
+}
+
+- (IBAction) saveFile: (id)sender {
+}
+
+- (void) appendToTimeCard: (NSString *) string {
+    NSRange end = {self.timeCardView.string.length, 0};
+    
+    [self.timeCardView replaceCharactersInRange: end withString: string];
+    
+    end = NSMakeRange(self.timeCardView.string.length, 0);
+    [self.timeCardView scrollRangeToVisible: end];
+}
+
+- (NSString *) currentDateTimeString
+{    
+    NSDateFormatter *utcDateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale *enUSPOSIXLocale = [[NSLocale alloc] initWithLocaleIdentifier: @"en_US_POSIX"];
+    
+    [utcDateFormatter setLocale: enUSPOSIXLocale];
+    [utcDateFormatter setDateFormat: @"yyyy'-'MM'-'dd'T'HH':'mm':'ss"];
+    [utcDateFormatter setTimeZone: [NSTimeZone localTimeZone]];
+    
+    NSString *dateTimeString = [utcDateFormatter stringFromDate:[NSDate date]];
+    
+    return dateTimeString;
+}
 @end
